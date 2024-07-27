fde5815f8f8992474069bb0459c8ee57181aeea8
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2024-07-27 13:10:47 -0600

    import_oidc_config env

--- {{cookiecutter.project_slug}}/backend/src/main/management/commands/import_oidc_config.py
+++ {{cookiecutter.project_slug}}/backend/src/main/management/commands/import_oidc_config.py
@@ -1,9 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db import transaction
 import os
 
 class Command(BaseCommand):
-    help = "Importa los catálogos desde archivos csv"
+    help = "Creates a new OIDC client from environment variables"
 
     def handle(self, *args, **options):
         from oidc_provider.models import Client, ResponseType
@@ -14,14 +14,14 @@ class Command(BaseCommand):
                 response_type, created = ResponseType.objects.get_or_create(value='code')
                 
                 # Crea el cliente
-                c = Client(name='nuxt', client_id=os.environ.get('OIDC_CLIENT_ID'), client_secret=os.environ.get('OIDC_CLIENT_SECRET'), redirect_uris=['http://localhost:3000/auth/oidc/callback'])
+                c = Client(name='frontend-{{cookiecutter.frontend}}', client_id=os.environ.get('OIDC_CLIENT_ID'), client_secret=os.environ.get('OIDC_CLIENT_SECRET'), redirect_uris=[os.environ.get('REDIRECT_URI')])
                 c.save()
                 
                 # Añade el ResponseType al cliente
                 c.response_types.add(response_type)
                 
-                self.stdout.write(self.style.SUCCESS('Cliente y ResponseType correctamente configurados.'))
+                self.stdout.write(self.style.SUCCESS('Created OIDC Client'))
         except ResponseType.DoesNotExist:
-            self.stdout.write(self.style.ERROR('ResponseType "code" no existe.'))
+            self.stdout.write(self.style.ERROR('ResponseType "code" does not exist'))
         except Exception as e:
-            self.stdout.write(self.style.ERROR(f'Error al configurar el Cliente y ResponseType: {e}'))
+            self.stdout.write(self.style.ERROR(f'Error while creating OIDC Client: {e}'))
