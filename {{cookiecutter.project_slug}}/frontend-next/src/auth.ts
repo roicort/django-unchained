0d641ed08dc58b77319debcca12507043f7ab733
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-03-09 21:25:24 -0600

    Update auth.ts

--- {{cookiecutter.project_slug}}/frontend-next/src/auth.ts
+++ {{cookiecutter.project_slug}}/frontend-next/src/auth.ts
@@ -2,26 +2,14 @@ import NextAuth from "next-auth";
 import type { OIDCConfig } from "@auth/core/providers";
 import axios from "axios";
 
-// Get the OIDC configuration from the well-known endpoint
-
-export const getOIDCConfig = async () => {
-  try {
-    const response = await axios.get(process.env.WELL_KNOWN_OIDC);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching OIDC configuration:", error);
-  }
-};
-
-const oidcConfig = await getOIDCConfig();
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     {
       id: "django",
       name: "Django Unchained",
       type: "oidc",
-      issuer: oidcConfig.issuer, 
+      issuer: process.env.OIDC_ISSUER, // from the provider's dashboard
+      wellKnown: process.env.OIDC_WELL_KNOWN, // from the provider's dashboard
       clientId: process.env.OIDC_CLIENT_ID, // from the provider's dashboard
       clientSecret: process.env.OIDC_CLIENT_SECRET, // from the provider's dashboard
     } satisfies OIDCConfig,
